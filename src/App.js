8252e9eb81a7ca4c638551d74ee637f35dc5ccde
launchpad5682 <launchpad5682@gmail.com>
2022-01-06 14:51:18 +0530

    chore: defautl files removed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Model from "./Model";
+import "./styles.css";
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <div>
+          <p>Use scroll or double finger gesture to zoom in and out</p>
+          <p>Revolve around axis using drag</p>
+        </div>
+        <Canvas>
+          <ambientLight intensity={0.5} />
+          <spotLight intensity={0.14} position={[0, 0, 0]} />
+          <Suspense fallback={null}>
+            <Model />
+          </Suspense>
+          <OrbitControls />
+        </Canvas>
+      </>
     </div>
   );
 }
